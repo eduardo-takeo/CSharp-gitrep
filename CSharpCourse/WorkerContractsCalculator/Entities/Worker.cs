d64ef37f396817f12a7a00709fb47e37d9b2160a
Eduardo Takeo <dudu.takeo96@gmail.com>
2019-02-11 12:38:13 -0200

    +WorkerContractsCalculator project

---
+++ CSharpCourse/WorkerContractsCalculator/Entities/Worker.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WorkerContractsCalculator.Entities.Enums;
+
+namespace WorkerContractsCalculator.Entities
+{
+    class Worker
+    {
+        public string Name { get; set; }
+        public WorkerLevel Level { get; set; }
+        public double BaseSalary { get; set; }
+        public Department Department { get; set; }
+        public List<HourContract> Contracts { get; set; } = new List<HourContract>();
+
+        public Worker()
+        {
+        }
+
+        public Worker(string name, WorkerLevel level, double baseSalary, Department department)
+        {
+            Name = name;
+            Level = level;
+            BaseSalary = baseSalary;
+            Department = department;
+        }
+    }
+}
