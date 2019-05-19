56a855823316ce2e043ed2f03a3e0681dac597c1
Eduardo Takeo <dudu.takeo96@gmail.com>
2019-05-19 15:33:17 -0300

    +CarRentWithoutInterface project

---
+++ CSharpCourse/CarRentWithoutInterface/Program.cs
@@ -0,0 +1,22 @@
+﻿using CarRentWithoutInterface.Entities;
+using System;
+using System.Globalization;
+
+namespace CarRentWithoutInterface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter rental data");
+            Console.WriteLine("Car Model: ");
+            string model = Console.ReadLine();
+            Console.WriteLine("Pickup (dd/MM/yyyy hh:mm): ");
+            DateTime start = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:ss", CultureInfo.InvariantCulture);
+            Console.WriteLine("Return (dd/MM/yyyy hh:mm): ");
+            DateTime finish = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:ss", CultureInfo.InvariantCulture);
+
+            CarRental carRental = new CarRental(start, finish, new Vehicle(model));
+        }
+    }
+}
---
+++ CSharpCourse/CarRentWithoutInterface/Services/RentalService.cs
@@ -0,0 +1,21 @@
+﻿using CarRentWithoutInterface.Entities;
+
+namespace CarRentWithoutInterface.Services
+{
+    class RentalService
+    {
+        public double PricePerHour { get; private set; }
+        public double PricePerDay { get; private set; }
+
+        public RentalService(double pricePerHour, double pricePerDay)
+        {
+            PricePerHour = pricePerHour;
+            PricePerDay = pricePerDay;
+        }
+
+        public void ProcessInvoice(CarRental carrental)
+        {
+
+        }
+    }
+}
