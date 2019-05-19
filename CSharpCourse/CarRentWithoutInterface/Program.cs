cb619d5241e0fbcf4c792dce77bd4637fbb217e8
Eduardo Takeo <dudu.takeo96@gmail.com>
2019-05-19 18:21:34 -0300

    Finishing the project

--- CSharpCourse/CarRentWithoutInterface/Program.cs
+++ CSharpCourse/CarRentWithoutInterface/Program.cs
@@ -1,6 +1,7 @@
 ﻿using CarRentWithoutInterface.Entities;
 using System;
 using System.Globalization;
+using CarRentWithoutInterface.Services;
 
 namespace CarRentWithoutInterface
 {
@@ -9,14 +10,26 @@ namespace CarRentWithoutInterface
         static void Main(string[] args)
         {
             Console.WriteLine("Enter rental data");
-            Console.WriteLine("Car Model: ");
+            Console.Write("Car Model: ");
             string model = Console.ReadLine();
-            Console.WriteLine("Pickup (dd/MM/yyyy hh:mm): ");
+            Console.Write("Pickup (dd/MM/yyyy hh:mm): ");
             DateTime start = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:ss", CultureInfo.InvariantCulture);
-            Console.WriteLine("Return (dd/MM/yyyy hh:mm): ");
+            Console.Write("Return (dd/MM/yyyy hh:mm): ");
             DateTime finish = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:ss", CultureInfo.InvariantCulture);
 
+            Console.WriteLine("Enter price per hour: ");
+            double hour = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Enter price per day: ");
+            double day = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
             CarRental carRental = new CarRental(start, finish, new Vehicle(model));
+
+            RentalService rentalService = new RentalService(hour, day);
+
+            rentalService.ProcessInvoice(carRental);
+
+            Console.WriteLine("INVOICE: ");
+            Console.WriteLine(carRental.Invoice);
         }
     }
 }
--- CSharpCourse/CarRentWithoutInterface/Services/RentalService.cs
+++ CSharpCourse/CarRentWithoutInterface/Services/RentalService.cs
@@ -1,4 +1,5 @@
 ﻿using CarRentWithoutInterface.Entities;
+using System;
 
 namespace CarRentWithoutInterface.Services
 {
@@ -7,15 +8,31 @@ namespace CarRentWithoutInterface.Services
         public double PricePerHour { get; private set; }
         public double PricePerDay { get; private set; }
 
+        private BrazilTaxService _brazilTaxService = new BrazilTaxService();
+
         public RentalService(double pricePerHour, double pricePerDay)
         {
             PricePerHour = pricePerHour;
             PricePerDay = pricePerDay;
         }
 
-        public void ProcessInvoice(CarRental carrental)
+        public void ProcessInvoice(CarRental carRental)
         {
+            TimeSpan duration = carRental.Finish.Subtract(carRental.Start);
 
+            double basicPayment = 0.0;
+            if (duration.TotalHours <= 12.0)
+            {
+                basicPayment = PricePerHour * Math.Ceiling(duration.TotalHours);
+            }
+            else
+            {
+                basicPayment = PricePerHour * Math.Ceiling(duration.TotalDays);
+            }
+
+            double tax = _brazilTaxService.Tax(basicPayment);
+
+            carRental.Invoice = new Invoice(basicPayment, tax);
         }
     }
 }
